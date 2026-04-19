80178bdb1a6ef46ee8ba33eb83ae990127cc7ab0
tmbkoren <>
2026-04-19 16:06:55 -0700

    fix

--- app/dashboard/actions.ts
+++ app/dashboard/actions.ts
@@ -1,8 +1,18 @@
+import { createClient } from "@/lib/supabase/server";
+
 export const getPagedOpportunities = async (page: number) => {
+    const supabase = await createClient();
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
     try {
         const backendUrl =
       process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000';
-    const res = await fetch(`${backendUrl}/api/opportunities?page=${page}`);
+    const res = await fetch(`${backendUrl}/api/opportunities?page=${page}`, {
+      headers: {
+        Authorization: `Bearer ${session.access_token}`,
+      },
+    });
     const data = await res.json();
     return data;
     } catch (error) {
